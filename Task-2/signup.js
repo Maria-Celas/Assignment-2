5e7595294a0cbb13845fa4ba758aae0b86f752fd
Maria-Celas <108416597+Maria-Celas@users.noreply.github.com>
2022-07-08 17:12:49 +0530

    Update signup.js

--- Task-2/signup.js
+++ Task-2/signup.js
@@ -14,80 +14,13 @@ const togglePassword = document.querySelector("#togglePassword");
 
 // Document is ready
 $(document).ready(function () {
-  // Validate Username
-  $("#usercheck").hide();
-  let usernameError = true;
-  $("#usernames").keyup(function () {
-    validateUsername();
-  });
- 
-  function validateUsername() {
-    let usernameValue = $("#usernames").val();
-    if (usernameValue.length == "") {
-      $("#usercheck").show();
-      usernameError = false;
-      return false;
-    } else if (usernameValue.length < 3 || usernameValue.length > 10) {
-      $("#usercheck").show();
-      $("#usercheck").html("**length of username must be between 3 and 10");
-      usernameError = false;
-      return false;
-    } else {
-      $("#usercheck").hide();
-    }
-  }
- 
-  // Validate Email
-  $("#emailcheck").hide();
-  let emailError = true;
-  $("#mail-id").keyup(function () {
-    validateEmail();
-  }); 
-  function validateEmail() {
-    let emailValue = $("#mail-id").val();
-    var email_regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (emailValue.length == "") {
-      $("#emailcheck").show();
-      emailError = false;
-      return false;
-    } else if (!emailValue.match(email_regex) ){
-      $("#emailcheck").show();
-      $("#emailcheck").html("**Please enter a valid email id");
-      emailError = false;
-      return false;
-    } else {
-      $("#emailcheck").hide();
-    }
-  }
- 
- 
-  // Validate Password
-  $("#passwordcheck").hide();
-  let passwordError = true;
-  $("#id_password").keyup(function () {
-    validatePassword();
-  });
-  function validatePassword() {
-    let passwordValue = $("#id_password").val();
-    if (passwordValue.length == "") {
-      $("#passwordcheck").show();
-      passwordError = false;
-      return false;
-    }
-    if (passwordValue.length < 3 || passwordValue.length > 10) {
-      $("#passwordcheck").show();
-      $("#passwordcheck").html(
-        "**length of your password must be between 3 and 10"
-      );
-      $("#passwordcheck").css("color", "red");
-      passwordError = false;
-      return false;
-    } else {
-      $("#passwordcheck").hide();
-    }
-  }
-// Submit button
-$("#submit_form").click(function () {
+
+  let usernameError = false;
+  let emailError = false;
+  let passwordError = false;
+
+  // Submit button
+  $("#submit_form").click(function () {
   validateUsername();
   validateEmail();
   validatePassword();
@@ -104,4 +37,74 @@ $("#submit_form").click(function () {
   //   return false;
   // }
 });
+
+    // Validate Username
+    $("#usercheck").hide();
+    $("#usernames").keyup(function () {
+      validateUsername();
+    });
+
+    function validateUsername() {
+      let usernameValue = $("#usernames").val();
+      if (usernameValue.length == "") {
+        $("#usercheck").show();
+
+        return false;
+      } else if (usernameValue.length < 3 || usernameValue.length > 10) {
+        $("#usercheck").show();
+        $("#usercheck").html("**length of username must be between 3 and 10");
+        
+        return false;
+      } else {
+        $("#usercheck").hide();
+        usernameError = true;
+      }
+    }
+
+    // Validate Email
+$("#emailcheck").hide();
+$("#mail-id").keyup(function () {
+  validateEmail();
+}); 
+function validateEmail() {
+  let emailValue = $("#mail-id").val();
+  var email_regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  if (emailValue.length == "") {
+    $("#emailcheck").show();
+    return false;
+  } else if (!emailValue.match(email_regex) ){
+    $("#emailcheck").show();
+    $("#emailcheck").html("**Please enter a valid email id");
+    return false;
+  } else {
+    $("#emailcheck").hide();
+    emailError = true;
+  }
+}
+
+ // Validate Password
+ $("#passwordcheck").hide();
+ $("#id_password").keyup(function () {
+   validatePassword();
+ });
+ function validatePassword() {
+   let passwordValue = $("#id_password").val();
+   if (passwordValue.length == "") {
+     $("#passwordcheck").show();
+    
+     return false;
+   }
+   if (passwordValue.length < 3 || passwordValue.length > 10) {
+     $("#passwordcheck").show();
+     $("#passwordcheck").html(
+       "**length of your password must be between 3 and 10"
+     );
+     $("#passwordcheck").css("color", "red");
+     
+     return false;
+   } else {
+     $("#passwordcheck").hide();
+     passwordError = true;
+   }
+ }
 });
